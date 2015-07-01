d06674ba101d8998a3af038b31753d7d58b3461d
therewasaguy <jason.sigal@gmail.com>
2015-06-30 22:52:39 -0400

    center map based on user location

--- ios/transients/transients/TZMapViewController.swift
+++ ios/transients/transients/TZMapViewController.swift
@@ -9,22 +9,73 @@
 import Foundation
 import UIKit
 import MapKit
+import CoreLocation
 
-class TZMapViewController : UIViewController{
+class TZMapViewController : UIViewController, CLLocationManagerDelegate{
     
     let mapView = MKMapView()
-    
+    let locationManager = CLLocationManager()
+
+    var mapScale = 0.25
+
+
     override func viewDidLoad() {
         super.viewDidLoad()
+
         // Do any additional setup after loading the view, typically from a nib.
         view.backgroundColor = UIColor.purpleColor()
         
-        let location = CLLocationCoordinate2D(latitude: 51.50007773, longitude: -0.1246402)
+        mapView.showsUserLocation = true
+
+        self.view.addSubview(mapView)
         
-        let span = MKCoordinateSpanMake(0.05, 0.05)
+        self.locationManager.delegate = self
+        self.locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        
+        // only request location when app is open
+        self.locationManager.requestWhenInUseAuthorization()
+        self.locationManager.startUpdatingLocation()
+    }
+
+
+    func locationManager(manager: CLLocationManager!, didUpdateLocations locations: [AnyObject]!) {
+//        println("\(manager.location.coordinate)")
+
+        self.centerMapView(manager.location.coordinate)
+
+        CLGeocoder().reverseGeocodeLocation(manager.location, completionHandler: {(placemarks, err)->Void in
+                if err != nil {
+                    println("Error: " + err.localizedDescription)
+                    return
+                }
+
+                if placemarks.count > 0 {
+                    let pm = placemarks[0] as! CLPlacemark
+                    self.displayLocationInfo(pm)
+                }
+            })
+    }
+
+    func displayLocationInfo(placemark: CLPlacemark) {
+            self.locationManager.stopUpdatingLocation()
+//            println(placemark.locality)
+//            println(placemark.postalCode)
+//            println(placemark.administrativeArea)
+//            println(placemark.country)
+    }
+
+    func locationManager(manager:CLLocationManager!, didFailWithError error: NSError!) {
+        println("Error: " + error.localizedDescription)
+    }
+
+    // center map based on user location
+    func centerMapView(coord: CLLocationCoordinate2D) {
+        let location = coord
+
+        let span = MKCoordinateSpanMake(self.mapScale, self.mapScale)
         
         let region = MKCoordinateRegionMake(location, span)
-    
+        
         mapView.frame = CGRectMake(0, 0, self.view.frame.size.width, self.view.frame.size.height)
         
         mapView.mapType = MKMapType.Standard
@@ -34,12 +85,15 @@ class TZMapViewController : UIViewController{
         mapView.scrollEnabled = true
         
         mapView.setRegion(region, animated: true)
-        
-        self.view.addSubview(mapView)
-        
+    }
     
-        
-        
-        
+
+    // re-center map based on location
+    func findMyLocationAgain() {
+        self.locationManager.startUpdatingLocation()
+    }
+
+    func mapViewChanged() {
+        // to do: reload pins based on the new map view
     }
 }
