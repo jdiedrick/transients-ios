b4fd54edb1872b3232d48bd2176d09e5d7651446
Johann Diedrick <jdiedrick@gmail.com>
2015-09-03 20:31:53 -0400

    covert recorded wav to mp3

---
+++ ios/transients/transients/AudioHelper.h
@@ -0,0 +1,13 @@
+//
+//  AudioHelper.h
+//  transients
+//
+//  Created by Johann Diedrick on 9/2/15.
+//  Copyright (c) 2015 Johann Diedrick. All rights reserved.
+//
+
+#ifndef transients_AudioHelper_h
+#define transients_AudioHelper_h
+
+
+#endif
---
+++ ios/transients/transients/AudioHelper.m
@@ -0,0 +1,9 @@
+//
+//  AudioHelper.m
+//  transients
+//
+//  Created by Johann Diedrick on 9/2/15.
+//  Copyright (c) 2015 Johann Diedrick. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
